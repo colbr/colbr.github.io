39f00bc23002548ff689ab73a43ed8b2778c6a2e
Brett <colbr243@gmail.com>
2021-04-29 02:57:14 +1200

    got rid of zero

--- zer0/static/js/14.0dd6affc.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpzns-dapp"]=this["webpackJsonpzns-dapp"]||[]).push([[14],{2448:function(p,s){}}]);
-//# sourceMappingURL=14.0dd6affc.chunk.js.map
--- zer0/static/js/15.1f7b5ac7.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpzns-dapp"]=this["webpackJsonpzns-dapp"]||[]).push([[15],{2572:function(p,s){}}]);
-//# sourceMappingURL=15.1f7b5ac7.chunk.js.map
